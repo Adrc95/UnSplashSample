522b16276ca6606d3d39c75e9c429c4f10cd13c2
Adrian <awinf204@gmail.com>
2022-10-11 17:15:21 +0200

    Actualizado librerias y gradle

--- app/src/main/java/com/adrc95/unsplashsample/di/AppModule.kt
+++ app/src/main/java/com/adrc95/unsplashsample/di/AppModule.kt
@@ -2,8 +2,6 @@ package com.adrc95.unsplashsample.di
 
 import android.app.Application
 import android.content.Context
-import com.adrc95.unsplashsample.ui.common.navigator.Navigator
-import com.adrc95.unsplashsample.ui.common.navigator.UnsplashAppNavigator
 import com.adrc95.usecase.Invoker
 import com.adrc95.usecase.UseCaseInvoker
 import dagger.Module
@@ -24,10 +22,5 @@ class AppModule {
     @Provides
     @Singleton
     fun providesInvoker(): Invoker = UseCaseInvoker()
-
-    @Provides
-    @Singleton
-    fun providesNavigator(context: Context): Navigator =
-        UnsplashAppNavigator(context)
 
 }
